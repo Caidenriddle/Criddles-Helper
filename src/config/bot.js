6fc5ed465127b3ba2492eacb0bea7f5fa2e71ac2
Caidenriddle <crazycaiden11@gmail.com>
2026-09-05 04:19:06 -0500

    Fix comment for bot presence status option

--- src/config/bot.js
+++ src/config/bot.js
@@ -5,7 +5,7 @@ export const botConfig = {
   // BOT PRESENCE (what users see under the bot name)
   // =========================
   // `status` options:
-  // - "online"    = green dot
+  // - "hello"    = green dot
   // - "idle"      = yellow moon
   // - "dnd"       = red do-not-disturb
   // - "invisible" = appears offline
